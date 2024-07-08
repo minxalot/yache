28b80f0332f78f4caad85a56fb0ebabe2f361e18
Fantastic Name <lebellaire@outlook.com>
2024-07-08 16:31:12 -0400

    testing asm music

---
+++ audio/music/NewSong.asm
@@ -0,0 +1,355 @@
+Music_printer2:
+	channel_count 4
+	channel 1, Music_printer2_Ch1
+	channel 2, Music_printer2_Ch2
+	channel 3, Music_printer2_Ch3
+	channel 4, Music_printer2_Ch4
+
+Music_printer2_Ch1:
+	tempo 256
+	volume 7, 7
+	note_type 12, 15, 8
+	rest 16
+	rest 10
+	octave 1
+	volume_envelope 2, 5
+	duty_cycle 3
+	transpose 0, 0
+	note B_, 1
+	rest 1
+	octave 2
+	note C#, 1
+	rest 1
+	note D#, 1
+	octave 8
+	rest 1
+.mainLoop:
+	octave 2
+	volume_envelope 2, 5
+	duty_cycle 3
+	transpose 0, 0
+	note E_, 1
+	rest 3
+	octave 1
+	note B_, 1
+	rest 3
+	octave 2
+	note E_, 1
+	rest 3
+	octave 1
+	note B_, 1
+	rest 3
+	octave 2
+	note E_, 1
+	rest 3
+	octave 1
+	note B_, 1
+	rest 3
+	octave 2
+	note E_, 1
+	rest 3
+	octave 1
+	note B_, 1
+	rest 1
+	octave 2
+	note E_, 1
+	rest 1
+	note F#, 1
+	rest 3
+	note C#, 1
+	rest 3
+	octave 1
+	note B_, 1
+	rest 3
+	note B_, 1
+	octave 2
+	rest 1
+	note C_, 1
+	rest 1
+	note C#, 1
+	rest 3
+	octave 1
+	note G#, 1
+	rest 3
+	note B_, 1
+	rest 3
+	octave 2
+	note E_, 1
+	rest 1
+	octave 1
+	note A#, 1
+	rest 1
+	note A_, 1
+	rest 3
+	octave 2
+	note E_, 1
+	rest 3
+	octave 1
+	note A_, 1
+	rest 3
+	octave 2
+	note E_, 1
+	rest 3
+	octave 1
+	note A_, 1
+	rest 3
+	octave 2
+	note C_, 1
+	rest 3
+	octave 1
+	note A_, 1
+	rest 3
+	octave 2
+	note C_, 1
+	rest 1
+	note E_, 1
+	rest 1
+	note F#, 1
+	rest 3
+	note D_, 1
+	rest 3
+	octave 1
+	note B_, 1
+	rest 1
+	note B_, 1
+	rest 1
+	octave 2
+	note D_, 1
+	rest 1
+	note D#, 1
+	rest 1
+	note E_, 1
+	rest 3
+	octave 1
+	note B_, 1
+	rest 3
+	octave 2
+	note E_, 1
+	rest 3
+	note E_, 1
+	rest 7
+	octave 1
+	volume_envelope 14, 7
+	note B_, 5
+	rest 3
+	note B_, 6
+	rest 8
+	volume_envelope 2, 5
+	note B_, 1
+	rest 1
+	octave 2
+	note C#, 1
+	note_type 12, 2, 5
+	rest 1
+	note D_, 1
+	rest 1
+	sound_loop 0, .mainLoop
+
+Music_printer2_Ch2:
+	note_type 12, 15, 8
+	rest 16
+	rest 16
+	volume_envelope 2, 8
+	octave 8
+.mainLoop:
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	note_type 12, 2, 8
+	sound_loop 0, .mainLoop
+
+Music_printer2_Ch3:
+	note_type 12, 2, 0
+	octave 4
+	vibrato 0, 0, 0
+	note B_, 6
+	octave 5
+	note C#, 1
+	rest 1
+	octave 4
+	note B_, 6
+	octave 5
+	note C#, 1
+	rest 1
+	octave 4
+	note B_, 1
+	rest 1
+	octave 5
+	note E_, 1
+	rest 1
+	note D#, 1
+	rest 1
+	note C#, 1
+	rest 1
+	octave 4
+	note B_, 1
+	rest 1
+	note A_, 1
+	rest 1
+	note G#, 1
+	rest 1
+	note F#, 1
+	rest 1
+	octave 8
+.mainLoop:
+	octave 4
+	note E_, 1
+	rest 1
+	octave 5
+	note E_, 1
+	note D#, 1
+	note E_, 1
+	rest 1
+	vibrato 0, 0, 0
+	note F#, 1
+	rest 3
+	note G#, 1
+	note F#, 1
+	note G#, 1
+	rest 1
+	note A_, 1
+	rest 3
+	note B_, 1
+	note A_, 1
+	note B_, 1
+	octave 6
+	note C#, 1
+	octave 5
+	note B_, 1
+	note A_, 1
+	note G#, 2
+	note G#, 1
+	note A_, 1
+	note G#, 1
+	note F#, 1
+	note E_, 2
+	rest 2
+	note C#, 1
+	note C_, 1
+	note C#, 1
+	rest 1
+	note D#, 1
+	rest 3
+	note E_, 1
+	note D#, 1
+	note E_, 1
+	rest 1
+	note F#, 1
+	rest 1
+	note E_, 3
+	note F#, 1
+	note E_, 1
+	rest 1
+	note C#, 1
+	rest 1
+	octave 4
+	note B_, 6
+	rest 4
+	octave 5
+	note C#, 1
+	note C_, 1
+	note C#, 1
+	rest 1
+	note D#, 1
+	rest 3
+	note E_, 1
+	note D#, 1
+	note E_, 1
+	rest 1
+	note F#, 1
+	rest 3
+	note G_, 1
+	note F#, 1
+	note E_, 1
+	note C_, 1
+	note E_, 1
+	note F#, 1
+	note G_, 2
+	note B_, 1
+	note G_, 1
+	note F#, 2
+	note E_, 2
+	note C#, 3
+	note D#, 1
+	note E_, 1
+	note D#, 1
+	note E_, 1
+	note F#, 1
+	note B_, 2
+	note A_, 1
+	pitch_slide 1, 5, A_
+	note B_, 1
+	note G#, 1
+	rest 1
+	note F#, 1
+	rest 1
+	note E_, 3
+	note F#, 1
+	note E_, 1
+	rest 1
+	octave 4
+	note B_, 1
+	rest 1
+	octave 5
+	note E_, 1
+	rest 3
+	pitch_slide 5, 6, E_
+	note B_, 1
+	rest 3
+	octave 4
+	note B_, 6
+	octave 5
+	note C#, 1
+	octave 4
+	rest 1
+	note B_, 6
+	octave 5
+	note C#, 1
+	rest 1
+	octave 4
+	note B_, 1
+	rest 1
+	octave 5
+	note E_, 1
+	rest 1
+	note D#, 1
+	rest 1
+	note C#, 1
+	rest 1
+	octave 4
+	note B_, 1
+	rest 1
+	note A_, 1
+	rest 1
+	note G#, 1
+	rest 1
+	note F#, 1
+	note_type 12, 2, 0
+	rest 1
+	sound_loop 0, .mainLoop
+
+Music_printer2_Ch4:
+	toggle_noise 0
+	drum_speed 12
+	rest 16
+	rest 16
+.mainLoop:
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	drum_speed 12
+	sound_loop 0, .mainLoop
